4847e9d10df82e147ae952e6106ef005cbca82b7
Rafael Callegari <137407464+CALLEGARii@users.noreply.github.com>
2024-03-07 21:49:59 -0300

    Add files via upload

--- Variáveis/script.js
+++ Variáveis/script.js
@@ -2,6 +2,24 @@
 
 let x = 10
 const y = 20
+
 console.log("Primeiro Número " + 10)
 console.log("Segundo número " + 20)
+
+console.log("\nSoma = " + (x + y))
+console.log("Subtração = " + (y - x))
+console.log("Divisão = " + (y / x))
+console.log("Multiplicação = " + (x * y))
+
+//Método um pouco Avançado//
+
+if(1){
+    const y = 20
+    console.log("\nNumero = " + 20)
+}
+
+if(2){
+    const x = 10
+    console.log("Numero = " + 10)
+}
 
