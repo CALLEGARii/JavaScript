33b481a98bef6bedea328ff0ceb7113c935bb0c8
Rafael Callegari <137407464+CALLEGARii@users.noreply.github.com>
2024-03-07 22:10:18 -0300

    Add files via upload

---
+++ Tipos de Dados/script.js
@@ -0,0 +1,42 @@
+//Tipos de Dados//
+
+const name = "Callegari"
+
+console.log(name)
+console.log("Tipo " + typeof name)
+///////////////////////////////////
+
+const qtdeCamisas = 4
+
+console.log("\n")
+console.log(qtdeCamisas)
+console.log("Tipo " + typeof qtdeCamisas)
+////////////////////////////////////////
+
+const decimal = 12.4
+
+console.log("\n")
+console.log(decimal)
+console.log("Tipo " + typeof decimal)
+////////////////////////////////////
+
+const isApproved = true
+console.log("\n")
+console.log(isApproved)
+console.log("Tipo " + typeof isApproved)
+///////////////////////////////////////
+
+let surname = null
+
+console.log("\n")
+console.log(surname)
+console.log("Tipo " + typeof surname)
+////////////////////////////////////
+
+surname = undefined
+
+console.log("\n")
+console.log(undefined)
+console.log("Tipo " + typeof surname)
+
+
