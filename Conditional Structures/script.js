81d111a9b9af355619ede7a416e28d97cd7a9bc7
Rafael Callegari <137407464+CALLEGARii@users.noreply.github.com>
2024-03-13 22:15:59 -0300

    Add files via upload

---
+++ Conditional Structures/script.js
@@ -0,0 +1,40 @@
+//Conditional Structures//
+
+const a = 10
+
+if(a > 8){
+
+    console.log("True")
+}else{
+    console.log("False")
+}
+
+////////////////////
+
+const b = "Callegari"
+
+if(b == "Joao"){
+    console.log("True")
+
+}else if(b == "Callegari"){
+
+    console.log("Esse é Lindao")
+    
+}else{
+    console.log("Nao encontrou o Matheus")
+}
+
+//Ternary//
+
+const nome = "Callegari"
+
+let testName = nome == "Joao" ? "True" : "False"
+console.log(testName)
+
+/////////////////
+
+const number = 25
+
+let testNumber = number <= 40 ? "True" : "False"
+console.log(testNumber)
+
